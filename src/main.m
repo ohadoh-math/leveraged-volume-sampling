5f09a108c4ee5c299a19deb30d7420fade24129d
Ohad Oh <not@an.email>
2019-02-19 18:05:54 +0200

    main now handles a single dataset and generates an output graph

--- src/main.m
+++ src/main.m
@@ -15,7 +15,7 @@ global script_file = [mfilename("fullpathext")];
 
 function [optimal_l2error, regression_time, ...
           lss_l2error, lss_l2error_std, lss_total_time ...
-          ] = handle_dataset(dataset_name, dataset_file, sampling_count, sample_sizes)
+          ] = handle_dataset(dataset_name, dataset_file, output_file, sampling_count, sample_sizes)
     % this function processes a single dataset file.
 
     % load the dataset into Octave.
@@ -37,29 +37,72 @@ function [optimal_l2error, regression_time, ...
                regression_time,
                optimal_l2error);
 
-    % perform leverage score sampling (abbreviated "lss")
-    info_trace("performing %d repetitions of leverage score sampling for %d sample sizes...", sampling_count, columns(sample_sizes));
-    for sample_sz = [30] % sample_sizes
+    lss_sl2errors = [];
+    lss_sl2errors_avg = [];
+    lss_sl2errors_std = [];
+
+    vss_sl2errors = [];
+    vss_sl2errors_avg = [];
+    vss_sl2errors_std = [];
+
+    lvss_sl2errors = [];
+    lvss_sl2errors_avg = [];
+    lvss_sl2errors_std = [];
+
+    optimal_error = optimal_l2error * ones(size(sample_sizes));
+
+    info_trace ("proceeding to sample via LSS, VSS and LVSS for %i tries each", sampling_count)
+    for sample_sz = sample_sizes
         info_trace("\t%s: LSS(k=%i, times=%i, n=%i, d=%i)", dataset_name, sample_sz, sampling_count, rows(X), columns(X))
         [sw, sXw, sl2error, sl2error_avg, sl2error_std, total_time] = naive_leverage_score_sampling(X, y, sample_sz, sampling_count);
 
-        printf("%s: LSS(k=%i, times=%i, n=%i, d=%i): sl2error=%f, ol2error=%f, sl2error_avg=%f[%f], time=%i secs\n",
-               dataset_name, sample_sz, sampling_count, rows(X), columns(X),
-               sl2error, optimal_l2error, sl2error_avg, sl2error_std,
-               floor(total_time));
+        info_trace("%s: LSS(k=%i, times=%i, n=%i, d=%i): sl2error=%f, ol2error=%f, sl2error_avg=%f[%f], time=%i secs\n",
+                   dataset_name, sample_sz, sampling_count, rows(X), columns(X),
+                   sl2error, optimal_l2error, sl2error_avg, sl2error_std,
+                   floor(total_time));
+        lss_sl2errors = [lss_sl2errors sl2error];
+        lss_sl2errors_avg = [lss_sl2errors sl2error_avg];
+        lss_sl2errors_std = [lss_sl2errors sl2error_std];
 
         info_trace("\t%s: VSS(s=%i, times=%i, n=%i, d=%i)", dataset_name, sample_sz, sampling_count, rows(X), columns(X));
         [sw, sXw, sl2error, sl2error_avg, sl2error_std] = volume_sampling(X, y, sample_sz, sampling_count);
-        printf("%s: VSS(s=%i, times=%i, n=%i, d=%i): sl2error=%f, ol2error=%f, sl2error_avg=%f[%f]\n",
-               dataset_name, sample_sz, sampling_count, rows(X), columns(X),
-               sl2error, optimal_l2error, sl2error_avg, sl2error_std);
+        info_trace("%s: VSS(s=%i, times=%i, n=%i, d=%i): sl2error=%f, ol2error=%f, sl2error_avg=%f[%f]\n",
+                   dataset_name, sample_sz, sampling_count, rows(X), columns(X),
+                   sl2error, optimal_l2error, sl2error_avg, sl2error_std);
+        vss_sl2errors = [vss_sl2errors sl2error];
+        vss_sl2errors_avg = [vss_sl2errors sl2error_avg];
+        vss_sl2errors_std = [vss_sl2errors sl2error_std];
 
         info_trace("\t%s: LVSS(k=%i, times=%i, n=%i, d=%i)", dataset_name, sample_sz, sampling_count, rows(X), columns(X));
         [sw, sXw, sl2error, sl2error_avg, sl2error_std] = leveraged_volume_sampling(X, y, sample_sz, sampling_count);
-        printf("%s: LVSS(s=%i, times=%i, n=%i, d=%i): sl2error=%f, ol2error=%f, sl2error_avg=%f[%f]\n",
-               dataset_name, sample_sz, sampling_count, rows(X), columns(X),
-               sl2error, optimal_l2error, sl2error_avg, sl2error_std);
+        info_trace("%s: LVSS(s=%i, times=%i, n=%i, d=%i): sl2error=%f, ol2error=%f, sl2error_avg=%f[%f]\n",
+                   dataset_name, sample_sz, sampling_count, rows(X), columns(X),
+                   sl2error, optimal_l2error, sl2error_avg, sl2error_std);
+        lvss_sl2errors = [lvss_sl2errors sl2error];
+        lvss_sl2errors_avg = [lvss_sl2errors sl2error_avg];
+        lvss_sl2errors_std = [lvss_sl2errors sl2error_std];
     end
+
+    info_trace("finished regressions - plotting results to %s", output_file);
+
+    graph_handle = figure();
+
+    min_x = min(sample_sizes);
+    max_x = max(sample_sizes);
+    min_y = 0.98*min([lss_sl2errors vss_sl2errors lvss_sl2errors optimal_error]);
+    max_y = 1.02*max([lss_sl2errors vss_sl2errors lvss_sl2errors optimal_error]);
+    axis([min_x max_x min_y max_y]);
+
+    plot (sample_sizes, lss_sl2errors, "-.xr;Leverage Scores Sampling;",
+          sample_sizes, vss_sl2errors, "--xb;Volume Sampling;",
+          sample_sizes, lvss_sl2errors, "-xg;Leveraged Volume Sampling;",
+          sample_sizes, optimal_error, "-.k;Full Regression;")
+
+    print(graph_handle, output_file, "-dpng");
+
+    save(strcat(output_file, ".octave"), "sample_sizes", "lss_sl2errors", "vss_sl2errors", "lvss_sl2errors", "optimal_error");
+
+    info_trace("done plotting!");
 
 endfunction
 
@@ -69,14 +112,42 @@ function _main
 
     global script_file;
 
-    % check that there's exactly at least one argument
-    if argc == 0
-        printf (["usage: %s DATASET-FILE[...]\n" ...
-                 "       Performs linear regression on a dataset file and print the error and time information."],
+    % check that there's at least:
+    %   1. dataset file
+    %   2. output file
+    %   3. at least one sample size
+    if argc < 3
+        printf (["usage: %s DATASET-FILE OUTPUT-FILE SAMPLE-SIZE[...]\n" ...
+                 "       Performs linear regression on a dataset via multiple sampling algorithms.\n" ...
+                 "       Generates a graph and saves it in PNG format.\n" ...
+                 "       * DATASET-FILE - the file holding the dataset. first column is assumed to be labels\n" ...
+                 "                        and the remaining columns are assumed to be features." ...
+                 "       * OUTPUT-FILE - the file to save the graph to." ...
+                 "       * SAMPLE-SIZE - a list of integers or comma-separated integers that indicate how many" ...
+                 "                       lines to sub-sample from the regression problem when using the various algorithms."],
                     script_file);
 
         exit (0);
     endif
+
+    dataset_file = argv{1};
+    output_file = argv{2};
+
+    % parse the sample sizes
+    sample_sizes = [];
+    for i=3:argc
+        separated_arg = strsplit(argv{i}, ",");
+        for j=1:length(separated_arg)
+            _sample_size = str2double(separated_arg{j});
+            if isnan(_sample_size)
+                error("invalid size '%s'", separated_arg{j})
+            else
+                sample_sizes = [sample_sizes floor(_sample_size)];
+            endif
+        endfor
+    endfor
+
+    sample_sizes = sort(unique(sample_sizes));
 
     % sadly, that's the most convenient way i know to pass arguments to an Octave script.
     % there doesn't seem to be something like python's `argparse` or `getopts`.
@@ -85,17 +156,11 @@ function _main
         sampling_count = 100;
     endif
 
-    % handle each dataset separately
-    for dataset_file = datasets
-        % extract just the file name from the data set file (that's the data set's name)
-        [s, e, te, matches, t, nm, sp] = regexp(dataset_file{1}, "([^/])+$");
-        dataset_name = matches{1};
+    % extract just the file name from the data set file (that's the data set's name)
+    [s, e, te, matches, t, nm, sp] = regexp(dataset_file, "([^/])+$");
+    dataset_name = matches{1};
 
-        [optimal_l2error, regression_time] = handle_dataset(dataset_name, dataset_file{1}, sampling_count, [30 40 50 60 70]);
-
-        printf ("%s\t%e\t%f\n", dataset_name, optimal_l2error, regression_time)
-    endfor
-
+    handle_dataset(dataset_name, dataset_file, output_file, sampling_count, sample_sizes);
 endfunction
 
 % execute `main()` on entry
